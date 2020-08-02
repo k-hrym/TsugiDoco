b8c070ade0bc90bd6996f511ff856f1a78e89aad
k-saunagi <k5.daha55@gmail.com>
2020-08-02 23:25:59 +0900

    tagモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_030231) do
+ActiveRecord::Schema.define(version: 2020_08_02_142253) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -99,6 +99,13 @@ ActiveRecord::Schema.define(version: 2020_07_19_030231) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_spots_on_place_id"
     t.index ["route_id"], name: "index_spots_on_route_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "place_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
