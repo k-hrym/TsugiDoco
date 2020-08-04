e34e34fac0ac6b2692d8dc426328f7f596d43964
k-saunagi <k5.daha55@gmail.com>
2020-08-05 01:32:31 +0900

    topページ修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def top
     @routes = Route.released.reverse_order.take(4)
-    @places = Place.all.reverse_order.take(5)
+    @places = Place.all.reverse_order.take(6)
   end
 
   def about
