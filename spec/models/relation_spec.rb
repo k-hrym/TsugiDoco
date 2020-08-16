dd52849875c6f6a12435a9618515ff8d470062e5
k-saunagi <k5.daha55@gmail.com>
2020-08-16 18:06:30 +0900

    relationのモデルテストを作成

--- spec/models/relation_spec.rb
+++ spec/models/relation_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Relation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    describe 'N:1の関係' do
+      context 'user_id' do
+        let(:target) { :user }
+        it { expect(association.macro).to eq :belongs_to }
+        it { expect(association.class_name).to eq 'User' }
+      end
+      context 'follow_id' do
+        let(:target) { :follow }
+        it { expect(association.macro).to eq :belongs_to }
+        it { expect(association.class_name).to eq 'User' }
+      end
+    end
+  end
 end
