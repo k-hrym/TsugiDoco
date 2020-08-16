69d6f594fd851adcea4109f713d1299dc7b1969e
k-saunagi <k5.daha55@gmail.com>
2020-08-16 17:35:52 +0900

    genreのモデルテストを作成

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    let(:genre) { create(:genre) }
+    subject{ genre.valid? }
+
+    context 'nameカラム' do
+      it '空欄ではないこと' do
+        genre.name = ''
+        is_expected.to eq false
+      end
+      it '空欄の場合はエラーを表示' do
+        genre.name = ''
+        genre.valid?
+        expect(genre.errors[:name]).to include('を入力してください')
+      end
+      it '10文字以内であること' do
+        genre.name = Faker::Lorem.characters(number: 11)
+        genre.valid?
+        expect(genre.errors[:name]).to include('は10文字以内で入力してください')
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    describe '1:Nの関係' do
+      context 'places' do
+        let(:target) { :places }
+        it { expect(association.macro).to eq :has_many }
+        it { expect(association.class_name).to eq 'Place' }
+      end
+    end
+  end
 end
