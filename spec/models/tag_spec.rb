2526c12b3c39907dbb87fe98a768212ac3aa567d
k-saunagi <k5.daha55@gmail.com>
2020-08-17 12:28:53 +0900

    tagのモデルテストを作成

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    describe 'N:1の関係' do
+      context 'place_image' do
+        let(:target) { :place_image }
+        it { expect(association.macro).to eq :belongs_to }
+        it { expect(association.class_name).to eq 'PlaceImage' }
+      end
+    end
+    describe '1:1の関係' do
+      context 'place' do
+        let(:target) { :place }
+        it { expect(association.macro).to eq :has_one }
+        it { expect(association.class_name).to eq 'Place' }
+      end
+    end
+  end
 end
