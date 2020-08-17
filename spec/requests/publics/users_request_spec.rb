7ffec7e76de05c2a7c6b897347e3cab7ac1f2ba4
k-saunagi <k5.daha55@gmail.com>
2020-08-17 23:17:05 +0900

    ユーザー詳細ページテスト

--- spec/requests/publics/users_request_spec.rb
+++ spec/requests/publics/users_request_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Publics::Users", type: :request do
+  let(:user) { create(:user) }
+  let(:another_user) { create(:user_2) }
+
   describe 'ログイン機能テスト' do
     # 存在するユーザー（DB登録ずみ）
     let(:existent_user) { create(:user) }
@@ -32,7 +35,9 @@ RSpec.describe "Publics::Users", type: :request do
   end
 
   describe '新規登録機能テスト' do
+    # 必要情報が入っている
     let(:req_params) {{user: {name: 'test-user', email: 'test@test.com', password: 'password', password_confirmation: 'password'}}}
+    # nameがブランクで必要情報が足りていない
     let(:name_blank_params) {{user: {name: '', email: 'test@test.com', password: 'password', password_confirmation: 'password'}}}
 
     context 'GET /users/sign_up' do
@@ -55,6 +60,82 @@ RSpec.describe "Publics::Users", type: :request do
         post user_registration_path, params: name_blank_params
         expect(response).to have_http_status(200)
         expect(response.body).to include('を入力してください')
+      end
+    end
+  end
+
+  describe 'ユーザー詳細(show)ページのテスト' do
+    describe 'ログインしている時' do
+      before { sign_in user }
+
+      context 'マイページにアクセス' do
+        before { get user_path user }
+        it '成功する' do
+          expect(response).to have_http_status(200)
+        end
+        it 'プロフィール編集リンクがある' do
+          expect(response.body).to include('プロフィール編集')
+        end
+      end
+
+      context '他のユーザーのページにログイン' do
+        before { get user_path another_user }
+        it '成功する' do
+          expect(response).to have_http_status(200)
+        end
+        it 'プロフィール編集リンクがない' do
+          expect(response.body).not_to include('プロフィール編集')
+        end
+      end
+    end
+
+    describe '未ログインの場合' do
+      context 'ユーザーページにアクセス' do
+        before { get user_path user }
+        it '成功する' do
+          expect(response).to have_http_status(200)
+        end
+        it 'プロフィール編集リンクがない' do
+          expect(response.body).not_to include('プロフィール編集')
+        end
+      end
+    end
+  end
+
+  describe 'ユーザー編集(edit)ページのテスト' do
+    describe 'ログインしている時' do
+      before { sign_in user }
+
+      context 'マイページにアクセス' do
+        before { get user_path user }
+        it '成功する' do
+          expect(response).to have_http_status(200)
+        end
+        it 'プロフィール編集リンクがある' do
+          expect(response.body).to include('プロフィール編集')
+        end
+      end
+
+      context '他のユーザーのページにログイン' do
+        before { get user_path another_user }
+        it '成功する' do
+          expect(response).to have_http_status(200)
+        end
+        it 'プロフィール編集リンクがない' do
+          expect(response.body).not_to include('プロフィール編集')
+        end
+      end
+    end
+
+    describe '未ログインの場合' do
+      context 'ユーザーページにアクセス' do
+        before { get user_path user }
+        it '成功する' do
+          expect(response).to have_http_status(200)
+        end
+        it 'プロフィール編集リンクがない' do
+          expect(response.body).not_to include('プロフィール編集')
+        end
       end
     end
   end
