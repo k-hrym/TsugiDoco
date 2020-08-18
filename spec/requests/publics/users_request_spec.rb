7e6451b9c0ec9b914a6e1e05c56080d83bdb773b
k-saunagi <k5.daha55@gmail.com>
2020-08-18 12:29:35 +0900

    [finish]ユーザーrequestテスト

--- spec/requests/publics/users_request_spec.rb
+++ spec/requests/publics/users_request_spec.rb
@@ -117,7 +117,7 @@ RSpec.describe "Publics::Users", type: :request do
         before { get edit_user_path another_user }
         it '失敗してtopページに飛ぶ' do
           expect(response).to redirect_to root_path
-          expect(response.body).not_to include('アクセス権限がありません')
+          expect(flash[:alert]).to include('アクセス権限がありません')
         end
       end
     end
@@ -125,40 +125,108 @@ RSpec.describe "Publics::Users", type: :request do
     describe '未ログインの場合' do
       context 'ユーザーページにアクセス' do
         before { get edit_user_path user }
-        it '失敗してtopページに飛ぶ' do
+        it '失敗してログインページに飛ぶ' do
           expect(response).to redirect_to new_user_session_path
-          expect(response.body).not_to include('アカウント登録もしくはログインしてください')
+          expect(flash[:alert]).to include('アカウント登録もしくはログインしてください')
         end
       end
     end
   end
 
-  describe 'ユーザー情報更新(update)ページのテスト' do
-    describe 'ログインしている時' do
-      before { sign_in user }
+  describe 'ユーザー情報更新(update)テスト' do
+    let(:user_params) {{name: '編集後',profile: '自己紹介文',birth: '1995/08/10'}}
 
-      context '自分の編集ページにアクセス' do
-        before { get edit_user_path  user }
-        it '成功する' do
-          expect(response).to have_http_status(200)
+    describe 'ログインしている時' do
+      before do
+        sign_in user
+      end
+
+      context '自分の情報を更新' do
+        before { patch user_path  user, user: user_params }
+        it 'リクエストが成功する' do
+          expect(response).to have_http_status(302)
+        end
+        it '更新が成功する' do
+          expect(user.reload.name).to eq '編集後'
+          expect(user.reload.profile).to eq '自己紹介文'
+          # expect(user.reload.birth).to eq Thu, 10 Aug 1995
+        end
+        it 'マイページにリダイレクト' do
+          expect(response).to redirect_to user_path user
         end
       end
 
-      context '他のユーザーのページにログイン' do
-        before { get edit_user_path another_user }
+      context '他のユーザーの情報を更新' do
+        before { patch user_path  another_user, user: user_params }
         it '失敗してtopページに飛ぶ' do
           expect(response).to redirect_to root_path
-          expect(response.body).not_to include('アクセス権限がありません')
+          expect(flash[:alert]).to include('アクセス権限がありません')
         end
       end
     end
 
     describe '未ログインの場合' do
       context 'ユーザーページにアクセス' do
-        before { get edit_user_path user }
+        before { patch user_path  user, user: user_params }
         it '失敗してtopページに飛ぶ' do
           expect(response).to redirect_to new_user_session_path
-          expect(response.body).not_to include('アカウント登録もしくはログインしてください')
+          expect(flash[:alert]).to include('アカウント登録もしくはログインしてください')
+        end
+      end
+    end
+  end
+
+  describe 'ユーザーのルート管理(route)ページテスト' do
+    let(:user_params) {{name: '編集後',profile: '自己紹介文',birth: '1995/08/10'}}
+
+    describe 'ログインしている時' do
+      before do
+        sign_in user
+      end
+
+      context '自分のルート管理ページにアクセス' do
+        before { get user_routes_path }
+        it '成功する' do
+          expect(response).to have_http_status(302)
+        end
+      end
+    end
+
+    describe '未ログインの場合' do
+      context 'ルート管理ページにアクセス' do
+        before { get user_routes_path }
+        it '失敗してログインページに飛ぶ' do
+          expect(response).to redirect_to new_user_session_path
+          expect(flash[:alert]).to include('アカウント登録もしくはログインしてください')
+        end
+      end
+    end
+  end
+
+  describe 'ユーザー退会アクション(hide)のテスト' do
+    describe 'ログインしている時' do
+      before do
+        sign_in user
+      end
+
+      context 'ステータスを退会に更新' do
+        it '成功する' do
+          patch users_hide_path user
+          expect(response).to have_http_status(302)
+          expect(user.reload.is_valid).to eq false
+          expect(flash[:notice]).to include('ご利用ありがとうございました。')
+          expect(session[:name]).to eq nil
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    describe '未ログインの場合' do
+      context 'ユーザー退会アクション' do
+        before { patch users_hide_path user }
+        it '失敗してログインページに飛ぶ' do
+          expect(response).to redirect_to new_user_session_path
+          expect(flash[:alert]).to include('アカウント登録もしくはログインしてください')
         end
       end
     end
