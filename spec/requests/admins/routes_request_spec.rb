60c094b7c1079f94573c5cee4a76636e90c4d6bb
k-saunagi <k5.daha55@gmail.com>
2020-08-19 23:25:18 +0900

    [finish]admins/routesのrequestテスト

--- spec/requests/admins/routes_request_spec.rb
+++ spec/requests/admins/routes_request_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Admins::Routes", type: :request do
+  let(:admin) { create(:admin) }
+  let(:route) { create(:route) }
+  before { sign_in admin }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admins/routes/index"
-      expect(response).to have_http_status(:success)
+  describe 'ルートの一覧(index)ページのテスト' do
+    context 'ルート一覧ページの表示' do
+      it '成功する' do
+        get admins_routes_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/admins/routes/show"
-      expect(response).to have_http_status(:success)
+  describe 'ルートの詳細(show)ページのテスト' do
+    context 'ルート詳細ページの表示' do
+      it '成功する' do
+        get admins_route_path route
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
