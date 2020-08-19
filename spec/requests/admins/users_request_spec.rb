eb795313fbf66cfef3f5a51202537e6e6cc3a9e8
k-saunagi <k5.daha55@gmail.com>
2020-08-19 23:35:15 +0900

    [finish]admins/usersのrequestテスト

--- spec/requests/admins/users_request_spec.rb
+++ spec/requests/admins/users_request_spec.rb
@@ -1,32 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Admins::Users", type: :request do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admins/users/index"
-      expect(response).to have_http_status(:success)
+  before { sign_in admin }
+
+  describe 'ユーザーの一覧(index)ページのテスト' do
+    context 'ユーザー一覧ページの表示' do
+      it '成功する' do
+        get admins_users_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/admins/users/show"
-      expect(response).to have_http_status(:success)
+  describe 'ユーザーの編集(edit)ページのテスト' do
+    context 'ユーザー編集ページの表示' do
+      it '成功する' do
+        get edit_admins_user_path user
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/admins/users/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/admins/users/update"
-      expect(response).to have_http_status(:success)
+  describe 'ユーザーの更新(update)機能のテスト' do
+    context 'ユーザー更新' do
+      it '成功する' do
+        patch admins_user_path(user.id),params: { user: { is_valid: false }}
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to admins_users_path
+        expect(flash[:notice]).to include('保存しました')
+      end
     end
   end
 
