8aad927ce9c963717da20e781092e1d148dbb258
k-saunagi <k5.daha55@gmail.com>
2020-08-20 00:14:16 +0900

    [finish]homeのrequestテスト

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
+  let(:user) { create(:user) }
 
+  describe 'ログインしている場合' do
+    before { sign_in user }
+    context 'topページの表示' do
+      it '成功する' do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+    context 'aboutページの表示' do
+      it '成功する' do
+        get about_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe 'ログインしていない場合も' do
+    context 'topページの表示' do
+      it '成功する' do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+    context 'aboutページの表示' do
+      it '成功する' do
+        get about_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 end
