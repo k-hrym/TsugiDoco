969b170ac8e3c7c0c1ca610d99bd5c5ff9cf6590
k-saunagi <k5.daha55@gmail.com>
2020-08-19 16:31:10 +0900

    [finish]wentのrequestテスト

--- spec/requests/publics/wents_request_spec.rb
+++ spec/requests/publics/wents_request_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Publics::Wents", type: :request do
+  let(:place) { create(:place) }
+  let(:user) { create(:user) }
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/publics/wents/create"
-      expect(response).to have_http_status(:success)
+  describe "ログイン済みの場合" do
+    before { sign_in user }
+    context 'いったをする', js: true do
+      it "成功する" do
+        post wents_path(id: place.id), xhr: true
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "いいねを取り消す" do
+      before do
+        post wents_path(id: place.id), xhr: true
+      end
+      it "成功する" do
+        delete wents_path(id: place.id), xhr: true
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/publics/wents/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
